881bcbf7a30815cf2ecce59a5b84f3f564a75577
Isaac Thill <ithill22@turing.edu>
2023-04-10 11:58:14 -0600

    refactor: simplify supermarkets_controller by creating new relationship with customers through customer_items

--- app/controllers/supermarkets_controller.rb
+++ app/controllers/supermarkets_controller.rb
@@ -1,8 +1,6 @@
 class SupermarketsController < ApplicationController
   def show
     @supermarket = Supermarket.find(params[:id])
-    @customer_items = CustomerItem.where(item_id: @supermarket.items.pluck(:id))
-    @customers = @customer_items.distinct.pluck(:customer_id)
-    @customer_records = Customer.where(id: @customers)
+    @customers = @supermarket.customers.distinct
   end
 end
