481dc2d8a300d4014678e73c33a3bf6fae11751c
Isaac Thill <ithill22@turing.edu>
2023-04-10 09:39:08 -0600

    complete user story 1

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 
+RSpec.describe 'Customer Show Page', type: :feature do
+  before :each do
+    test_data
+  end
+
+  describe 'As a visitor, when I visit the customer show page' do
+    it 'I see the customers name ane a list of its items' do
+      visit "/customers/#{@customer_1.id}"
+
+      expect(page).to have_content(@customer_1.name)
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_2.name)
+      expect(page).to have_content(@item_1.price)
+      expect(page).to have_content(@item_2.price)
+      expect(page).to have_content(@item_1.supermarket.name)
+    end
+  end
+end
